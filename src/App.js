e301b6a602e3ee1ba3b7ce333c0fe35146113bd6
JuKad <kadlecovajuliana@gmail.com>
2021-02-18 15:04:39 +0100

    animista trial run

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>
+         Weather App
+      </h1>
+      <footer>
+        This project was coded by Juliana Kadlecova and is {" "}
+        <a 
+          href="https://github.com/JuKad/jk-react-weather-app"
+          target="_blank" >
+          open-sourced on GibHub.
+        </a>
+      </footer>
     </div>
   );
 }
